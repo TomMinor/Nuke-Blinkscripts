9811c6967efeebbdb9e0e2bdc73b410016e236d9
Tom Minor <tom.minor@FWZ840004.london.thefoundry.co.uk>
2016-11-16 10:31:01 +0000

    Added ambient occlusion and tweaked the lighting a bit

--- sdf_primitives.h
+++ sdf_primitives.h
@@ -12,3 +12,11 @@ float sdBox(float3 _p, float3 _bounds)
 
 	return min(b, 0.0f) + length( max(d, 0.0f) );
 }
+
+
+float sdPlane( float3 p, float4 n )
+{
+	float3 tmpN = float3( n.x, n.y, n.z );
+  	// n must be normalized
+  	return dot(p, tmpN) + n.w;
+}
